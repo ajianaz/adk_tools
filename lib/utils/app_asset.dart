fac959330330374c8f773e54a3dacb4dc56aead7
Anaz S. Aji <aji.anaz@gmail.com>
2024-04-25 11:07:28 +0700

    Update app asset - add custom folder

--- lib/utils/app_asset.dart
+++ lib/utils/app_asset.dart
@@ -10,4 +10,8 @@ class AppAsset {
   static String image(String s) {
     return 'assets/images/$s';
   }
+
+  static String custom(String folder, String s) {
+    return 'assets/$folder/$s';
+  }
 }
